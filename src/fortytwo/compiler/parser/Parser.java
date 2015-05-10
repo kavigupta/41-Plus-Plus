5b83197588ff57116f9c93a8b515b1b0e1d1558e
Kavi Gupta <kavi@globalmbox.com>
2015-05-10 13:13:00 -0700

    Implemented tokenizer, which takes a paragraph and converts it into 'tokens' which can be more easily parsed

--- src/fortytwo/compiler/parser/Parser.java
+++ src/fortytwo/compiler/parser/Parser.java
@@ -1,30 +1,20 @@
 package fortytwo.compiler.parser;
 
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import fortytwo.compiler.errors.SyntaxMatchingError;
 import fortytwo.compiler.struct.Statement42;
 
 public class Parser {
-	public static final Pattern LINES = Pattern.compile("(?<line>[^\\.]+)\\.");
 	private Parser() {}
-	public static ArrayList<Statement42> interpret(String program)
-			throws SyntaxMatchingError {
-		ArrayList<Statement42> statements = new ArrayList<>();
-		Matcher mat = LINES.matcher(program);
-		while (mat.find()) {
-			statements.add(intepretLine(collapseWhitespace(mat.group("line"))));
-		}
-		return statements;
-	}
 	public static String collapseWhitespace(String line) {
 		return line.replaceAll("\\s+", " ");
 	}
 	public static Statement42 intepretLine(String line)
 			throws SyntaxMatchingError {
-		ArrayList<String> tokens = smartTokenize(line);
+		List<String> tokens = tokenize42(line);
 		switch (tokens.get(0)) {
 			case "Define":
 				return parseDefinition(tokens);
@@ -35,16 +25,108 @@ public class Parser {
 		}
 		// TODO more types of statements
 	}
-	private static Statement42 parseDefinition(ArrayList<String> tokens) {
+	private static Statement42 parseDefinition(List<String> tokens) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	private static Statement42 parseAssignment(ArrayList<String> tokens) {
+	private static Statement42 parseAssignment(List<String> tokens) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	private static ArrayList<String> smartTokenize(String line) {
-		// TODO Auto-generated method stub
-		return null;
+	public static List<String> tokenize42(String text) {
+		text = collapseWhitespace(text).trim() + " ";
+		boolean inString = false;
+		ArrayList<String> tokens = new ArrayList<>();
+		StringBuffer sbuff = new StringBuffer();
+		for (int i = 0; i < text.length(); i++) {
+			if (inString) {
+				if (text.charAt(i) == '\\') {
+					i++;
+					switch (text.charAt(i)) {
+						case '\\':
+							sbuff.append('\\');
+							continue;
+						case 'n':
+							sbuff.append('\n');
+							continue;
+						case 'r':
+							sbuff.append('\r');
+							continue;
+						case 'b':
+							sbuff.append('\b');
+							continue;
+						case 't':
+							sbuff.append('\t');
+							continue;
+						case 'f':
+							sbuff.append('\f');
+							continue;
+						case '\'':
+							sbuff.append('\'');
+							continue;
+						case 'u':
+							try {
+								int u = Integer.parseInt(
+										text.substring(i + 1, i + 5),
+										16);
+								sbuff.append((char) (u));
+								i = i + 4;
+							} catch (NumberFormatException e) {
+								throw new RuntimeException(/* TODO */);
+							}
+							continue;
+						default:
+							throw new RuntimeException(/* TODO */);
+					}
+				}
+				sbuff.append(text.charAt(i));
+				if (text.charAt(i) == '\'') inString = false;
+				continue;
+			}
+			switch (text.charAt(i)) {
+				case '\'':
+					inString = sbuff.length() == 0;
+					sbuff.append(text.charAt(i));
+					break;
+				case '(':
+					if (sbuff.length() != 0)
+						throw new RuntimeException(/* TODO */);
+					int index = text.indexOf(')', i);
+					if (index < 0) throw new RuntimeException(/* TODO */);
+					tokens.add(text.substring(i, index + 1));
+					i = index;
+					break;
+				case ')':
+					throw new RuntimeException(/* TODO */);
+				case '[':
+					index = text.indexOf(']', i);
+					if (index < 0) throw new RuntimeException(/* TODO */);
+					i = index;
+					break;
+				case ']':
+					throw new RuntimeException(/* TODO */);
+				case ':':
+				case '.':
+				case ';':
+				case ',':
+					if (text.charAt(i + 1) == ' ') {
+						tokens.add(sbuff.toString());
+						sbuff.setLength(0);
+						sbuff.append(text.charAt(i));
+					} else {
+						sbuff.append(text.charAt(i) + "");
+					}
+					break;
+				case ' ':
+					tokens.add(sbuff.toString());
+					sbuff.setLength(0);
+					break;
+				default:
+					sbuff.append(text.charAt(i));
+			}
+		}
+		if (inString) throw new RuntimeException(/* TODO */);
+		return tokens.stream().filter(s -> s.length() != 0)
+				.collect(Collectors.toList());
 	}
 }
--- test/fortytwo/test/ParserTest.java
+++ test/fortytwo/test/ParserTest.java
@@ -2,20 +2,47 @@ package fortytwo.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Test;
 
-import fortytwo.compiler.errors.SyntaxMatchingError;
 import fortytwo.compiler.parser.Parser;
 
 public class ParserTest {
 	@Test
-	public void lineDefinitionTest() throws SyntaxMatchingError {
-		checkParse("Define a number called x");
-		checkParse("Define a number called x with a value of 2");
-		checkParse("Set the value of x to 2");
-		checkParse("Define an array called data with a length of (the value of x)");
+	public void tokenizerTest() {
+		assertEquals(new ArrayList<String>(Arrays.asList()),
+				Parser.tokenize42(""));
+		assertEquals(
+				Arrays.asList("Define", "a", "number", "called", "_x", "."),
+				Parser.tokenize42("Define a number called _x."));
+		assertEquals(
+				Arrays.asList("If", "4.0", "+", "4.0", "=", "2", ":",
+						"Dothis", ";", "otherwise", ":", "Dothat", "."),
+				Parser.tokenize42("If 4.0 + 4.0 = 2: Dothis; otherwise: Dothat."));
+		assertEquals(
+				Arrays.asList("Define", "a", "string", "called", "_x",
+						"with", "a", "value", "of", "'()[]'\r\n'", "."),
+				Parser.tokenize42("Define a string called _x with a value of '()[]\\'\\r\\n'."));
+		assertEquals(
+				Arrays.asList("Define", "a", "number", "called", "_x", "."),
+				Parser.tokenize42("Define a [COMPILER ERROR HERE ''''''''''''] number called _x."));
+		assertEquals(Arrays.asList("Set", "the", "value", "of", "_x", "to",
+				"(1+2*3)", "."),
+				Parser.tokenize42("Set the value of _x to (1+2*3)."));
+		assertEquals(
+				Arrays.asList("_technically_this_is_a_valid_variable_name'"),
+				Parser.tokenize42("_technically_this_is_a_valid_variable_name'"));
 	}
-	private void checkParse(String toParse) throws SyntaxMatchingError {
-		assertEquals(toParse, Parser.intepretLine(toParse).toString());
-	}
+	// @Test
+	// public void lineDefinitionTest() throws SyntaxMatchingError {
+	// checkParse("Define a number called x");
+	// checkParse("Define a number called x with a value of 2");
+	// checkParse("Set the value of x to 2");
+	// checkParse("Define an array called data with a length of (the value of x)");
+	// }
+	// private void checkParse(String toParse) throws SyntaxMatchingError {
+	// assertEquals(toParse, Parser.intepretLine(toParse).toString());
+	// }
 }
