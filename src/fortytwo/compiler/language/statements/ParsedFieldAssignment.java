bf44958000050c7a6a08e4101ffa29294af0b236
Kavi Gupta <kavi@globalmbox.com>
2015-05-15 17:26:17 -0700

    Clocking in. Will finish refactor

--- src/fortytwo/compiler/language/statements/ParsedFieldAssignment.java
+++ src/fortytwo/compiler/language/statements/ParsedFieldAssignment.java
@@ -7,12 +7,12 @@ import fortytwo.vm.statements.Statement;
 
 public class ParsedFieldAssignment implements ParsedAssignment {
 	public final VariableIdentifier name, field;
-	public final ParsedExpression parseExpression;
+	public final ParsedExpression value;
 	public ParsedFieldAssignment(VariableIdentifier name,
 			VariableIdentifier field, ParsedExpression parseExpression) {
 		this.name = name;
 		this.field = field;
-		this.parseExpression = parseExpression;
+		this.value = parseExpression;
 	}
 	@Override
 	public Statement contextualize(LocalEnvironment environment) {
