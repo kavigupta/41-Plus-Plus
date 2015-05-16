e6215d22276468c75d72c066648ab408d323f5b5
Kavi Gupta <kavi@globalmbox.com>
2015-05-15 22:08:05 -0700

    Will now properly implement Functions.

--- src/fortytwo/vm/constructions/Function42.java
+++ src/fortytwo/vm/constructions/Function42.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import lib.standard.collections.Pair;
 import fortytwo.compiler.language.identifier.VariableIdentifier;
-import fortytwo.vm.environment.GlobalEnvironment;
 import fortytwo.vm.expressions.Expression;
 import fortytwo.vm.expressions.LiteralExpression;
 
 public interface Function42 {
-	public LiteralExpression apply(GlobalEnvironment env,
+	public LiteralExpression apply(LocalEnvironment env,
 			List<Pair<VariableIdentifier, Expression>> arguments);
 }
