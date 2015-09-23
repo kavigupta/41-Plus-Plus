6508651010cafbc77665da43671104075885a3e9
root <root@kavi-HP-Ubuntu>
2015-09-23 00:11:25 -0700

    Adding test cases

---
+++ 41++ Test Suite/test/fortytwo/test/ExitPlacementTest.java
@@ -0,0 +1,45 @@
+package fortytwo.test;
+
+import org.junit.Test;
+
+import fortytwo.compiler.Context;
+import fortytwo.vm.errors.ErrorType;
+
+public class ExitPlacementTest extends EnvironmentTester {
+	public static final String NO_EXIT_ERROR = ""//
+			+ "Define a function called the absolute value of \"x\" that takes a number called \"x\" and outputs a number.\n"
+			+ "	If \"x\" is greater than 0:\n"
+			+ "		Exit the function and output \"x\".";
+	public static final String NO_EXIT_WORKS = ""//
+			+ "Define a function called the absolute value of \"x\" that takes a number called \"x\" and outputs a number.\n"
+			+ "	If \"x\" is greater than 0:\n"//
+			+ "		Exit the function and output \"x\".\n"//
+			+ "	Otherwise:\n"//
+			+ "		Exit the function and output -\"x\".";
+	public static final String CONTROL_PAST_END = ""//
+			+ "Define a function called the absolute value of \"x\" that takes a number called \"x\" and outputs a number.\n"
+			+ "	Exit the function and output \"x\".\n"
+			+ "	Set the value of \"x\" to 2.";
+	@Test
+	public void testExitFlow() {
+		Utilities.assertError(ErrorType.TYPING,
+				"The function \"the absolute value of <arg>:(function that takes a number and outputs a number)\" does not output under some cases",
+				0, CONTROL_PAST_END.length(),
+				() -> loadEnvironment(NO_EXIT_ERROR),
+				Context.entire(NO_EXIT_ERROR));
+		try {
+			loadEnvironment(NO_EXIT_WORKS);
+		} catch (Throwable t) {
+			throw new AssertionError("Expected the loading to work properly",
+					t);
+		}
+	}
+	@Test
+	public void testNoControlPastEnd() {
+		Utilities.assertError(ErrorType.TYPING,
+				"The function \"the absolute value of <arg>:(function that takes a number and outputs a number)\" contains statements past the end",
+				0, CONTROL_PAST_END.length(),
+				() -> loadEnvironment(CONTROL_PAST_END),
+				Context.entire(CONTROL_PAST_END));
+	}
+}
